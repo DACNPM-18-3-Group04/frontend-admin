1e977a921b1839a258700067519de54c655883b3
T N <truong1320vv@gmail.com>
2022-05-13 10:34:47 +0700

    get all report

--- src/helpers/api/contact/reviewReport.js
+++ src/helpers/api/contact/reviewReport.js
@@ -1,8 +1,8 @@
 import api, { getAuthConfig } from '..';
 const baseURL = '/contact/review/report';
 
-export const getReviewReportList = (limit = 5) =>
-  api.get(`${baseURL}?limit=${limit}`, getAuthConfig());
+export const getReviewReportList = () =>
+  api.get(`${baseURL}/all`, getAuthConfig());
 
 export const updateReportStatus = (reportID, stx = 'S') =>
   api.put(`${baseURL}?report_id=${reportID}`, { status: stx }, getAuthConfig());
